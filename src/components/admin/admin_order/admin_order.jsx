a8b01f1cbf38e58da6e424f26e6fc4539a2edbe8
Jeongbin <yon980824@gmail.com>
2022-06-20 05:26:42 +0900

    Product order component

--- src/components/admin/admin_order/admin_order.jsx
+++ src/components/admin/admin_order/admin_order.jsx
@@ -13,12 +13,14 @@ import {
 } from "firebase/firestore";
 import { ref, uploadString, getDownloadURL } from "@firebase/storage";
 import adminStyles from "../admin.module.css";
-import styles from "..//admin_product/admin_product.module.css";
-// import Admin_products_item from "./admin_products_item";
+import styles from "../admin_product/admin_product.module.css";
+import Admin_products_item from "../admin_product/admin_products_item";
+import Admin_order_item from "./admin_order_item";
 
 const Admin_order = () => {
-  // 의자 데이터 로드
+  // 주문 데이터 로드
   const [items, setItems] = useState("");
+  const [totalCount, setTotalCount] = useState(0);
   const getItems = async () => {
     const q = query(collection(dbService, "order"));
     onSnapshot(q, (snapshot) => {
@@ -39,7 +41,47 @@ const Admin_order = () => {
     console.log(items);
   }, [items]);
 
-  return <div>as</div>;
+  return (
+    <>
+      <section className={adminStyles.section}>
+        <div className={adminStyles.sectionHeader}>
+          <h2 className={adminStyles.sectionTitle}>등록 제품</h2>
+        </div>
+        <div
+          className={`${adminStyles.sectionBody} ${styles.productContainer}`}
+        >
+          <table className={styles.productTable}>
+            <thead>
+              <tr>
+                <th>선택</th>
+                <th>이미지</th>
+                <th>제품명</th>
+                <th>가격</th>
+                <th>렌탈가</th>
+                <th className={styles.hide}>날짜</th>
+                <th className={styles.hide}>작성자</th>
+              </tr>
+            </thead>
+            <tbody>
+              {items
+                ? items.map((chair) => (
+                    <Admin_order_item item={chair} key={chair.id} />
+                  ))
+                : null}
+            </tbody>
+
+            <tfoot>
+              <tr>
+                <td colSpan="2">제품 수</td>
+                <td>{totalCount}</td>
+                {/* <td colSpan="3">페이지</td> */}
+              </tr>
+            </tfoot>
+          </table>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Admin_order;
---
+++ src/components/admin/admin_order/admin_order_item.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSquareMinus } from "@fortawesome/free-regular-svg-icons";
+import styles from "../admin_product/admin_product.module.css";
+import { dbService, storageService } from "../../../service/firebase";
+import { doc, deleteDoc } from "firebase/firestore";
+import { deleteObject, ref } from "firebase/storage";
+
+const Admin_order_item = ({ item }) => {
+  let createdDate = new Date(item.createdAt);
+  createdDate =
+    createdDate.getFullYear() +
+    "-" +
+    (createdDate.getMonth() + 1) +
+    "-" +
+    createdDate.getDate() +
+    " " +
+    createdDate.getHours() +
+    ":" +
+    createdDate.getMinutes();
+
+  // 아이템 삭제
+  const onDeleteClick = async () => {
+    const ok = window.confirm("해당 데이터를 삭제하시겠습니까?");
+    console.log(ok);
+    if (ok) {
+      const itemObj = doc(dbService, "chair", `${item.id}`);
+      await deleteDoc(itemObj);
+      if (item.item.imgURL !== "") {
+        await deleteObject(ref(storageService, item.item.imgURL));
+      }
+    }
+  };
+
+  return (
+    <tr key={item.id}>
+      <td className={styles.deletBtn}>
+        <button onClick={onDeleteClick}>
+          <FontAwesomeIcon icon={faSquareMinus} />
+        </button>
+      </td>
+      <td>
+        <img width="30" src={item.item.imgURL} alt="productImg" />
+      </td>
+      <td className={styles.name}>{item.item.name}</td>
+      <td>{item.item.price.toLocaleString()}</td>
+      <td>{item.item.rentPrice.toLocaleString()}</td>
+      <td className={styles.hide}>{createdDate}</td>
+      <td className={styles.hide}>{item.creatorId}</td>
+    </tr>
+  );
+};
+
+export default Admin_order_item;
